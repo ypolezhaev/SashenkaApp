3df5bd976e7c6e9701a858e94f327cae66a6a907
Yakov <ypolezhaev@timepath.ru>
2025-08-03 23:01:49 +0500

    #Положение картинок хедера относительно блока а не экрана

--- frontend/src/components/perfomance/Perfomance.jsx
+++ frontend/src/components/perfomance/Perfomance.jsx
@@ -16,38 +16,49 @@ function ParfomanceForm() {
 
     const imageStyle = { opacity, scale, y: translateY};
     return (
-        <Box as="section" className='PerfomanceContainer' position='relative'>
-            <Container className='TextContainer'>
-                <Flex direction={'column'} align={'flex-start'} gap={0.4} >
-                    <Container padding={0} maxWidth={'1200px'}>
-                        <Flex direction={'row'} gap={'11px'}>
-                            <Text zIndex={11} className={'LargeText black'}>привет,</Text>
-                            <Text zIndex={11} className={'LargeText black'}>меня зовут</Text>
-                        </Flex>
-                    </Container>
-                    <Container textAlign={'start'} maxWidth={'771px'} >
-                        <Text zIndex={11} className={'LargeText black'}>александра</Text>
-                    </Container>
-                    <Container textAlign={'start'} maxWidth={'530px'} >
-                        <Flex direction={'row'}>
-                            <Text zIndex={11} className={'LargeText black'}>и я&nbsp;</Text>
-                            <Text zIndex={11} className={'LargeText red'}>графический</Text>
-                        </Flex>
-                    </Container>
-                    <Container textAlign={'start'} maxWidth={'530px'} >
-                        <Text zIndex={11} className={'LargeText red'}>дизайнер</Text>
-                    </Container>
-                </Flex>
+        <Box as="section" className='PerfomanceContainer'>
+            <Container px={0} className='PerfomanceContentContainer'>
+                <Container className='TextContainer'>
+                    <Flex direction={'column'} align={'flex-start'} gap={0.4} >
+                        <Container padding={0} maxWidth={'1200px'}>
+                            <Flex direction={'row'} gap={'11px'}>
+                                <Text zIndex={11} className={'LargeText black'}>привет,</Text>
+                                <Text zIndex={11} className={'LargeText black'}>меня зовут</Text>
+                            </Flex>
+                        </Container>
+                        <Container textAlign={'start'} maxWidth={'771px'} >
+                            <Text zIndex={11} className={'LargeText black'}>александра</Text>
+                        </Container>
+                        <Container textAlign={'start'} maxWidth={'530px'} >
+                            <Flex direction={'row'}>
+                                <Text zIndex={11} className={'LargeText black'}>и я&nbsp;</Text>
+                                <Text zIndex={11} className={'LargeText red'}>графический</Text>
+                            </Flex>
+                        </Container>
+                        <Container textAlign={'start'} maxWidth={'530px'} >
+                            <Text zIndex={11} className={'LargeText red'}>дизайнер</Text>
+                        </Container>
+                    </Flex>
+                </Container>
+                <Container px={0} top={'117px'} left={'0px'} maxWidth={'200px'} maxHeight={'200px'} position={'absolute'}>
+                    <MotionImage zIndex={-1} style={imageStyle} initial={{rotate: 19}}  src='./cube.svg'/>
+                </Container>
+                <Container px={0} top={'24px'} left={'1106px'} maxWidth={'160px'} maxHeight={'160px'} position={'absolute'} >
+                    <MotionImage zIndex={-1} style={imageStyle} src='./cursor.svg'/>
+                </Container>
+                <Container px={0} top={'335px'} left={'1161px'} maxWidth={'210px'} maxHeight={'210px'} position={'absolute'}>
+                    <MotionImage zIndex={-1} style={imageStyle} initial={{rotate: 346}} src='./strawberry.svg'/>
+                </Container>
+                <Container px={0} top={'334px'} left={'83px'} maxWidth={'250px'} maxHeight={'250px'} position={'absolute'}>
+                    <MotionImage zIndex={-1} style={imageStyle} initial={{rotate: 307}} src='./gl.svg'/>
+                </Container>
+                <Container px={0} top={'473px'} left={'380px'} maxWidth={'200px'} maxHeight={'200px'} position={'absolute'}>
+                    <MotionImage zIndex={-1} style={imageStyle} initial={{rotate: 355}} src='./mug.svg'/>
+                </Container>
+                <Container px={0} top={'400px'} left={'730px'} maxWidth={'230px'} maxHeight={'230px'} position={'absolute'}>
+                    <MotionImage zIndex={-1} style={imageStyle} initial={{rotate: 13}} src='./constructor.svg'/>
+                </Container>
             </Container>
-            <Box>
-                
-            </Box>
-            <MotionImage zIndex={-1} style={imageStyle} initial={{rotate: 19}} top={'117px'} left={'252.5px'} maxWidth={'200px'} maxHeight={'200px'} position={'absolute'} src='./cube.svg'/>
-            <MotionImage zIndex={-1} style={imageStyle} top={'24px'} left={'1358.5px'} maxWidth={'160px'} maxHeight={'160px'} position={'absolute'} src='./cursor.svg'/>
-            <MotionImage zIndex={-1} style={imageStyle} initial={{rotate: 346}} top={'369.5px'} left={'1413.5px'} maxWidth={'210px'} maxHeight={'210px'} position={'absolute'} src='./strawberry.svg'/>
-            <MotionImage zIndex={-1} style={imageStyle} initial={{rotate: 307}} top={'369.5px'} left={'335.5px'} maxWidth={'250px'} maxHeight={'250px'} position={'absolute'} src='./gl.svg'/>
-            <MotionImage zIndex={-1} style={imageStyle} initial={{rotate: 355}} top={'473px'} left={'632.5px'} maxWidth={'200px'} maxHeight={'200px'} position={'absolute'} src='./mug.svg'/>
-            <MotionImage zIndex={-1} style={imageStyle} initial={{rotate: 13}} top={'400px'} left={'982.5px'} maxWidth={'230px'} maxHeight={'230px'} position={'absolute'} src='./constructor.svg'/>
         </Box>
     );
 }
